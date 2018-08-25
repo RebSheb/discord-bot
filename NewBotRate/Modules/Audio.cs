ff2918698ca43460fec26c8ecef3d07bd4c52789
Rob <robxplo98@outlook.com>
2018-08-25 23:54:46 +0100

    Added audio.cs for preperation to do stuff

---
+++ NewBotRate/Modules/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using NAudio;
+using Discord;
+using Discord.Commands;
+
+namespace NewBotRate.Modules
+{
+    [Group("audio")]
+    public class Audio : ModuleBase<SocketCommandContext>
+    {
+        
+        [Command("volumeup"), Summary("Earrape a file")]
+        public async Task IncreaseVolume([Summary("The amount to boost the volume by")] float increase)
+        {
+
+
+            return;
+        }
+        
+    }
+}
